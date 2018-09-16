1e04dd8f1a6cf7a59982ba0d37e07bfaaf2ba571
Stephane Cusin <stephane.cusin@wayoos.com>
2018-09-16 22:48:37 +0200

    generate endpoints

--- initial-cluster.sh
+++ initial-cluster.sh
@@ -4,15 +4,23 @@
 set -e
 
 cluster_size=$1
-kube_id="de1"
+kube_id=$2
+
 initial_cluster=""
+endpoints=""
 
 function etcd_node() {
-    local node_id="etcd$1-$kube_id"
+    local node_id="$kube_id.etcd$1"
     echo "$node_id=http://$node_id.wayoos.net:2380"
 }
 
+function etcd_endpoint() {
+    local node_id="$kube_id.etcd$1"
+    echo "http://$node_id.wayoos.net:2379"
+}
+
 initial_cluster=$(etcd_node 0)
+endpoints=$(etcd_endpoint 0)
 
 COUNTER=1
 while [  $COUNTER -lt $cluster_size ]; do
@@ -20,8 +28,10 @@ while [  $COUNTER -lt $cluster_size ]; do
     node=$(etcd_node $COUNTER)
     initial_cluster="$initial_cluster,$node"
 
+    endpoint=$(etcd_endpoint $COUNTER)
+    endpoints="$endpoints,$endpoint"
+
     let COUNTER=COUNTER+1 
 done
 
-
-echo "{\"initial-cluster\":\"$initial_cluster\"}"
+echo "{\"initial-cluster\":\"$initial_cluster\",\"endpoints\":\"$endpoints\"}"
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "initial-cluster" {
   value       = "${data.external.initial-cluster.result["initial-cluster"]}"
   description = "Initial cluster"
 }
+
+output "endpoints" {
+  value = "${data.external.initial-cluster.result["endpoints"]}"
+}
